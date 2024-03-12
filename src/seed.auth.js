0e8b65d96bd72eef4978328a12c891a3805a0aa4
Julio Flores <juliocesarflores12@gmail.com>
2024-03-12 06:07:43 -0400

    remove: login from empty seed

--- src/seed.auth.js
+++ src/seed.auth.js
@@ -1,5 +1,6 @@
 const { HttpAgent } = require("@dfinity/agent");
 const { Ed25519KeyIdentity } = require("@dfinity/identity");
+const { AuthClient } = require("@dfinity/auth-client");
 
 /**
  * @class SeedAuth
@@ -42,10 +43,18 @@ class SeedAuth {
     return false;
   }
 
-  _identityFromEmptySeed(seed) {
-    // TODO: generate a new identity from an empty seed, as an anonymous principal 2vxsx-fae
-  }
-
+  /**
+   * Generates an Ed25519 key identity from a provided seed.
+   *
+   * This function throws an error if the seed is empty or invalid.
+   *
+   * @param {string} seed - The seed to use for generating the identity.
+   * @returns {Ed25519KeyIdentity} - The generated Ed25519 key identity.
+   * @memberof SeedAuth
+   * @example
+   * SeedAuth.seedToIdentity(seed);
+   * // => Ed25519KeyIdentity
+   */
   static seedToIdentity(seed) {
     if (this._isSeedEmpty(seed)) return this._identityFromEmptySeed(seed);
 
@@ -57,8 +66,24 @@ class SeedAuth {
     return Ed25519KeyIdentity.generate(seedBuffer);
   }
 
+  /**
+   * Creates a new HTTP Agent instance.
+   *
+   * This function requires either a seed or an identity to be provided.
+   * If seed is provided, it will be used to generate an identity using the `seedToIdentity` function.
+   *
+   * @param {object} options - Options for creating the agent.
+   * @param {string} options.seed - The seed to use for generating the identity (optional).
+   * @param {Ed25519KeyIdentity} options.identity - The Ed25519 key identity to use (optional).
+   * @param {string} [options.host] - The hostname of the server to connect to (optional).
+   * @returns {HttpAgent} - The newly created HttpAgent instance.
+   * @memberof SeedAuth
+   * @example
+   * SeedAuth.create
+   * // => HttpAgent
+   */
   static async createAgent(options) {
-    if (!options.seed || !options.identity)
+    if (!options.seed && !options.identity)
       throw new Error("Seed or Identity is required");
     const { seed, identity } = options;
 
@@ -68,6 +93,7 @@ class SeedAuth {
       return agent;
     }
 
+    const agent = new HttpAgent({ identity, host: options?.host });
     return agent;
   }
 }
